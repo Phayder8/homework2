8558956f1e91358c794516953fd9fb18c6874148
Phayder8 <witcher2567@gmail.com>
2024-06-28 13:03:31 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+homework_completed = 12
+hours_spent = 1.5
+сourse_name = "Python"
+time_per_task = hours_spent/homework_completed
+print(time_per_task)
+print("Курс:",сourse_name,", Всего задач:",homework_completed,", Затрачено часов:",hours_spent,", Cреднее время выполнения:",time_per_task,".")
