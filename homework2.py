d9a4a69a6d5ac83e97596be32494526a888d276c
Phayder8 <witcher2567@gmail.com>
2024-06-28 13:03:13 +0300

    Delete homework2.py

--- homework2.py
+++
@@ -1,6 +0,0 @@
-homework_completed = "12"
-hours_spent = "1.5"
-сourse_name = "Python"
-print(1.5/12)
-time_per_task = "0.125"
-print("Курс:",сourse_name,", Всего задач:",homework_completed,", Затрачено часов:",hours_spent,", Cреднее время выполнения:",time_per_task,".")
